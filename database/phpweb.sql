81c2d6fc2744050528c11a2356bb7ebc05916590
1246864 <jiangyunzhe2008@qq.com>
2025-12-11 01:00:57 +0800

    feat(数据库): 添加sys_routes表和初始化用户数据
    - 创建sys_routes表用于路由配置
    - 添加默认路由规则和测试用户数据

--- database/phpweb.sql
+++ database/phpweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2025 年 12 月 11 日 00:24
+-- 生成日期: 2025 年 12 月 11 日 01:00
 -- 服务器版本: 5.6.21
 -- PHP 版本: 5.4.34
 
@@ -23,6 +23,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 表的结构 `sys_routes`
+--
+
+CREATE TABLE IF NOT EXISTS `sys_routes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键ID',
+  `url` varchar(255) NOT NULL COMMENT '路由路径(记得带斜杠)',
+  `file_path` varchar(255) NOT NULL DEFAULT 'index.php' COMMENT '对应页面路径',
+  `method` varchar(20) NOT NULL DEFAULT 'ALL' COMMENT '请求方式',
+  `is_enable` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否启用',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`),
+  UNIQUE KEY `id` (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+
+--
+-- 转存表中的数据 `sys_routes`
+--
+
+INSERT INTO `sys_routes` (`id`, `url`, `file_path`, `method`, `is_enable`) VALUES
+(1, '/', 'index.php', 'ALL', 1),
+(2, '/index', 'index.php', 'ALL', 1);
+
+-- --------------------------------------------------------
+
+--
 -- 表的结构 `user`
 --
 
@@ -37,7 +62,14 @@ CREATE TABLE IF NOT EXISTS `user` (
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `email` (`email`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 转存表中的数据 `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `role`, `email`, `image`) VALUES
+(1, 'j2yz', '$2a$12$hxapcrW/hWOi0mnNLFYyTuKHuCc29eSvVQIkRoM3yM52cITpT5XFe', 'user', '1278633', 'default');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
